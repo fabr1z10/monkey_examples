b8583b0cfa7a4acaa7d239c7a3717ad25ce3c90e
Fabrizio <fabrizio.venturini@optrail.com>
2023-10-27 21:59:23 +0200

    added pipe

--- smba/game/builder.py
+++ smba/game/builder.py
@@ -13,3 +13,20 @@ def object(**kwargs):
         return monkey_toolkit.platformer.tiled(x, y, settings.models[kwargs.get('model')])
     return f
 
+
+def pipe(**kwargs):
+    def f(x, y, height):
+        assert (height > 2)
+        model = {
+            'sheet': 'mario',
+            'size': [4, height],
+            'animations': {
+                'main': {
+                    'frames': [
+                        { 'data': [0x50000+kwargs.get('pal'), 0x10000 + (height-2), 38, 39, 134, 40, 0x20000, 166, 167, 168, 169, 102, 103, 104, 105]}
+                    ]
+                }
+            }
+        }
+        return monkey_toolkit.platformer.tiled(x, y, model, platform=1)
+    return f
